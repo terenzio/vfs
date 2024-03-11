7886f2461e811d1d3b1b72d4242f96d1e993ba07
terenzio <terenzio@gmail.com>
2024-03-11 15:56:48 +0800

    [Feature] completed IO scanner with error checking for unrecoznised commands

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -9,6 +12,38 @@ func main() {
 	now := time.Now()
 	timeString := now.Format(time.DateTime)
 
-	fmt.Println("Hello IsCoolLab! Lets build a Virtual File System!")
-	fmt.Println("Current time in string format:", timeString)
+	fmt.Println("==== IsCoolLab: Virtual File System CLI ====")
+	fmt.Println("The current time is:", timeString)
+	fmt.Println("Type 'help' to see available commands.")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+
+		fmt.Print("# ")
+
+		if !scanner.Scan() {
+			break // Exit the loop if an error occurs or EOF is reached
+		}
+
+		input := scanner.Text()
+		args := strings.Fields(input)
+
+		switch args[0] {
+		case "exit":
+			fmt.Println("Exiting program.")
+			return
+		case "help":
+			fmt.Println("Available commands:")
+			fmt.Println("register [username]")
+		default:
+			fmt.Println("Error: Unrecognized command. Type 'help' to see available commands.")
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "reading standard input: %v\n", err)
+		}
+
+	}
+
 }
